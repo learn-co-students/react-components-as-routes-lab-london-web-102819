e290c41a5a26aa74654c3d7f91edb11bb98f3325
github username <github email address>
2020-01-18 12:08:54 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1>Actors Page</h1>
+            {actors.map((a, idx) => (
+                <div key = {idx}>
+                    <h3>{a.name}</h3>
+                    <ul>
+                        {a.movies.map((m, idx) => <li key = {idx}>{m}</li>)}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    );
+}
 
 export default Actors;
