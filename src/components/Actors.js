fb13ec7dadce1937834c46d84978f99fe3663128
PianoFF <Fangfei1star@gmail.com>
2020-01-27 15:49:37 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
+
+const Actor = ({ name, movies }) => (
+  <div>
+    <h1> {name} </h1>
+    <ul>
+      {movies.map(movie => (
+        <li> {movie}</li>
+      ))}
+    </ul>
+  </div>
+);
 
 const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(Actor)}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="navbar">
       {/*{code here}*/}
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/movies" exact>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" exact>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" exact>
+        Actors
+      </NavLink>
     </div>
   );
 };
